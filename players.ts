650df4eb4e617313fcd192c37f9e9dc46cc5af87
Nehal Chauhan <98140974+mRcOol7@users.noreply.github.com>
2025-02-11 17:43:02 +0530

    Add files via upload

---
+++ players.ts
@@ -0,0 +1,236 @@
+export const tiers = [
+  {
+    id: 'mvp',
+    name: 'MVP',
+    color: 'bg-purple-600',
+    description: 'Most Valuable Player'
+  },
+  {
+    id: 'goated',
+    name: 'GOATED',
+    color: 'bg-yellow-500',
+    description: 'Greatest of All Time'
+  },
+  {
+    id: 'decent',
+    name: 'Decent',
+    color: 'bg-green-500',
+    description: 'Above Average'
+  },
+  {
+    id: 'solid',
+    name: 'Solid',
+    color: 'bg-blue-500',
+    description: 'Reliable Performance'
+  },
+  {
+    id: 'meh',
+    name: 'Meh',
+    color: 'bg-orange-500',
+    description: 'Average Performance'
+  },
+  {
+    id: 'ass',
+    name: 'Ass',
+    color: 'bg-red-500',
+    description: 'Needs Improvement'
+  },
+  {
+    id: 'dnp',
+    name: 'DNP',
+    color: 'bg-gray-500',
+    description: 'Did Not Play'
+  }
+] as const;
+
+export const players = [
+  // Legends
+  
+    {
+      "id": "1",
+      "name": "Lionel Messi",
+      "image": "https://fcb-abj-pre.s3.amazonaws.com/img/jugadors/MESSI.jpg",
+      "era": "legend"
+    },
+    {
+      "id": "2",
+      "name": "Xavi Hernández",
+      "image": "https://fcb-abj-pre.s3.amazonaws.com/img/jugadors/949_xavi.jpg",
+      "era": "legend"
+    },
+    {
+      "id": "3",
+      "name": "Andrés Iniesta",
+      "image": "https://fcb-abj-pre.s3.amazonaws.com/img/jugadors/INIESTA%202017-18.jpg",
+      "era": "legend"
+    },
+    {
+      "id": "4",
+      "name": "Carles Puyol",
+      "image": "https://fcb-abj-pre.s3.amazonaws.com/img/jugadors/704_puyol.jpg",
+      "era": "legend"
+    },
+    {
+      "id": "5",
+      "name": "Johan Cruyff",
+      "image": "https://fcb-abj-pre.s3.amazonaws.com/img/jugadors/213_cruyff.jpg",
+      "era": "legend"
+    },
+    {
+      "id": "6",
+      "name": "Ronaldinho",
+      "image": "https://fcb-abj-pre.s3.amazonaws.com/img/jugadors/763_ronaldinho.jpg",
+      "era": "legend"
+    },
+    {
+      "id": "7",
+      "name": "Samuel Eto'o",
+      "image": "https://fcb-abj-pre.s3.amazonaws.com/img/jugadors/278_etoo.jpg",
+      "era": "legend"
+    },
+  
+  
+
+  // Current Players
+  
+    {
+      "id": "8",
+      "name": "Marc-André ter Stegen",
+      "image": "https://www.fcbarcelona.com/photo-resources/2024/10/13/04c3b761-ccbd-498f-b988-b67ed1693906/01-Ter_Stegen-M.png?width=670&height=790",
+      "era": "current"
+    },
+    {
+      "id": "9",
+      "name": "Pau Cubarsí",
+      "image": "https://www.fcbarcelona.com/photo-resources/2024/10/13/951e72f0-59ae-4685-be81-f2afced7e6d6/02-Cubarsi-M.png?width=670&height=790",
+      "era": "current"
+    },
+    {
+      "id": "10",
+      "name": "Alejandro Balde",
+      "image": "https://www.fcbarcelona.com/photo-resources/2024/10/13/e3784cf8-9f03-45e2-ab44-007f2adadf32/03-Balde-M.png?width=670&height=790",
+      "era": "current"
+    },
+    {
+      "id": "11",
+      "name": "Ronald Araújo",
+      "image": "https://www.fcbarcelona.com/photo-resources/2024/10/13/5352ecab-dc7a-4d20-a047-67af419dd96b/04-Araujo-M.png?width=670&height=790",
+      "era": "current"
+    },
+    {
+      "id": "12",
+      "name": "Iñigo Martínez",
+      "image": "https://www.fcbarcelona.com/photo-resources/2024/10/13/3129af5f-0ec1-4474-988c-1415092eb74f/05-Martinez-M.png?width=670&height=790",
+      "era": "current"
+    },
+    {
+      "id": "13",
+      "name": "Gavi",
+      "image": "https://www.fcbarcelona.com/photo-resources/2024/10/13/545795b7-4c63-4347-94ab-5f2a49b1eddd/06-Gavi-M.png?width=670&height=790",
+      "era": "current"
+    },
+    {
+      "id": "14",
+      "name": "Ferran Torres",
+      "image": "https://www.fcbarcelona.com/photo-resources/2024/10/13/1edefda1-8229-4232-ace9-2f35bf4d3000/07-Ferran_Torres-M.png?width=670&height=790",
+      "era": "current"
+    },
+    {
+      "id": "15",
+      "name": "Pedri",
+      "image": "https://www.fcbarcelona.com/photo-resources/2024/10/13/61d91e06-7435-4823-93e3-2ef7e91b3c8b/08-Pedri-M.png?width=670&height=790",
+      "era": "current"
+    },
+    {
+      "id": "16",
+      "name": "Robert Lewandowski",
+      "image": "https://www.fcbarcelona.com/photo-resources/2024/10/13/894e4e5e-b6d0-434e-8610-890e0971a7b2/09-Lewandowski-M.png?width=670&height=790",
+      "era": "current"
+    },
+    {
+      "id": "17",
+      "name": "Ansu Fati",
+      "image": "https://www.fcbarcelona.com/photo-resources/2024/10/13/a21d9ed5-ab8a-4705-ae9a-a6bd14e9dbba/10-Ansu_Fati-M.png?width=670&height=790",
+      "era": "current"
+    },
+    {
+      "id": "18",
+      "name": "Raphinha",
+      "image": "https://www.fcbarcelona.com/photo-resources/2024/10/13/266dc5af-16b0-4290-99c5-03d73a48df11/11-Raphinha-M.png?width=670&height=790",
+      "era": "current"
+    },
+    {
+      "id": "19",
+      "name": "Iñaki Peña",
+      "image": "https://www.fcbarcelona.com/photo-resources/2024/10/13/c058dc89-fa0b-4aa9-99de-a129ece52320/13-Inaki_Pena-M.png?width=670&height=790",
+      "era": "current"
+    },
+    {
+      "id": "20",
+      "name": "Pablo Torre",
+      "image": "https://www.fcbarcelona.com/photo-resources/2024/10/13/0b7225dc-8ec1-4957-bb92-3ebd72f00dd7/14-Pablo_Torre-M.png?width=670&height=790",
+      "era": "current"
+    },
+    {
+      "id": "21",
+      "name": "Andreas Christensen",
+      "image": "https://www.fcbarcelona.com/photo-resources/2024/10/13/0969d283-c3f8-471a-8638-9a37124ea1a0/15-Christensen-M.png?width=670&height=790",
+      "era": "current"
+    },
+    {
+      "id": "22",
+      "name": "Fermín López",
+      "image": "https://www.fcbarcelona.com/photo-resources/2024/10/13/bc22e993-3083-4ef5-aa77-217951756cca/16-Fermin-M.png?width=670&height=790",
+      "era": "current"
+    },
+    {
+      "id": "23",
+      "name": "Marc Casadó",
+      "image": "https://www.fcbarcelona.com/photo-resources/2024/10/13/d9fb4b17-1260-48a5-841d-ffd2a60e57e4/17-Casado-M.png?width=670&height=790",
+      "era": "current"
+    },
+    {
+      "id": "24",
+      "name": "Pau Víctor",
+      "image": "https://www.fcbarcelona.com/photo-resources/2024/10/13/64d7a058-2043-4a39-a5b6-2b7525541fd5/18-Pau_Victor-M.png?width=670&height=790",
+      "era": "current"
+    },
+    {
+      "id": "25",
+      "name": "Lamine Yamal",
+      "image": "https://www.fcbarcelona.com/photo-resources/2024/10/13/d942d5c0-5ada-423c-baf1-4d2ba5ebe3bb/19-Lamine-M.png?width=670&height=790",
+      "era": "current"
+    },
+    {
+      "id": "26",
+      "name": "Dani Olmo",
+      "image": "https://www.fcbarcelona.com/photo-resources/2024/10/13/6df53816-c907-4ecc-bd0a-c2e8e5a281c2/20-Olmo-M.png?width=670&height=790",
+      "era": "current"
+    },
+    {
+      "id": "27",
+      "name": "Frenkie de Jong",
+      "image": "https://www.fcbarcelona.com/photo-resources/2024/10/13/1f6c10a5-07cd-47b3-929b-e29ccabe18d7/21-De_Jong-M.png?width=670&height=790",
+      "era": "current"
+    },
+    {
+      "id": "28",
+      "name": "Jules Koundé",
+      "image": "https://www.fcbarcelona.com/photo-resources/2024/10/13/9f7d8d15-0244-4def-a931-8eb563a598a1/23-Kounde-M.png?width=670&height=790",
+      "era": "current"
+    },
+    {
+      "id": "29",
+      "name": "Eric García",
+      "image": "https://www.fcbarcelona.com/photo-resources/2024/10/13/60ec100e-ca62-492c-a3c7-7e73956b035b/24-Eric_Garcia-M.png?width=670&height=790",
+      "era": "current"
+    },
+    {
+      "id": "30",
+      "name": "Wojciech Szczęsny",
+      "image": "https://www.fcbarcelona.com/photo-resources/2024/10/14/a62932d1-106c-4762-84bf-89fa9a633c71/25-Szczesny-M.png?width=670&height=790",
+      "era": "current"
+    }
+  
+  
+] as const;
